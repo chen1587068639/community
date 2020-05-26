d3d2a64310b6c7996c00ee8d43cb64bfc3f04c07
chen1587068639 <1587068639@qq.com>
2020-05-26 19:21:39 +0800

    first commit

---
+++ src/main/java/com/chen/springboot/Application.java
@@ -0,0 +1,14 @@
+package com.chen.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//spring项目启动入口类
+@SpringBootApplication      //spring核心注解，主要用于开发spring自动配置x`
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
